5acfb6f8404ab84409c48e792d3647802d3bec7a
kevicebryan <kevinbryanreligion@gmail.com>
2026-04-09 23:00:03 +1000

    Update theme.ts

--- lib/theme.ts
+++ lib/theme.ts
@@ -1,7 +1,22 @@
 import { createTheme } from "@mantine/core";
 
 export const pokemuTheme = createTheme({
-  primaryColor: "orange",
+  primaryColor: "mistral",
+  primaryShade: { light: 6, dark: 6 },
+  colors: {
+    mistral: [
+      "#FFF7DC",
+      "#FFEFAD",
+      "#FFE57A",
+      "#FFD800",
+      "#FFAF00",
+      "#FF8205",
+      "#FA500F",
+      "#E5480E",
+      "#A3320A",
+      "#8b1f00",
+    ],
+  },
   fontFamily: '"Syne Mono", "Trebuchet MS", Arial, sans-serif',
   headings: {
     fontFamily: '"Handjet", "Courier New", monospace',
